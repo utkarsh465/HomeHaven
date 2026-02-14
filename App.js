b9213142146f387032b1cc654ecc19ffd01f7cdb
Utkarsh Raj <Utkarshr571@gmail.com>
2026-02-15 00:44:00 +0530

    new route

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const Listing = require("./models/listing");
 
@@ -17,6 +18,12 @@ main().then(() => {
 async function main (){
     await mongoose.connect(Mongo_Url);
 }
+
+
+// this line is for ejs files
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
 
 app.get("/",(req,res)=>{
     res.send("i am root");
@@ -37,14 +44,40 @@ app.get("/",(req,res)=>{
 //     res.send("Sample listing created");
 // })
 
+
+
+// index route
 app.get("/listings",async (req,res) =>{
-    const allListings = await Listings.find({});
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs",{allListings});
+});
+
+// New route
+
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs")
 })
 
+// Show route
 
+app.get("/listings/:id",async(req,res)=>{
+    let{id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
+});
+
+
+// Create Route
+
+app.post("/listings",async(req,res)=>{
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+})
 
 
 
 app.listen("8080",()=>{
     console.log("server is working");
 })
+
